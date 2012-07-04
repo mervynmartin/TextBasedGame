5c7a35c86c33959500e72fb603168acc17298b84
mervynmartin <mervyn.martin@gmail.com>
2012-07-04 19:30:22 -0400

    fixed client issue preventing passing of password

--- service/login.php
+++ service/login.php
@@ -5,8 +5,7 @@ header('Content-type: application/json');
 include 'lib\LoginController.inc';
 
 	$username = isset($_POST['username']) ? $_POST['username'] : '';
-	$password = isset($_POST['password']) ? $_POST['password'] : ''; 
-	
+	$password = isset($_POST['password']) ? $_POST['password'] : '';
 
 	if(LoginController::login($username, $password))
 	{
